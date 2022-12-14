a929748d84ea055cd07c0b54278ee4e9be7d4236
Lyudmila Balakireva <ludab@pn2300050.lanl.gov>
2022-12-14 15:57:47 -0700

    added thread InputMain

--- portals-crawler/src/main/java/gov/lanl/crawler/input/InputMain.java
+++ portals-crawler/src/main/java/gov/lanl/crawler/input/InputMain.java
@@ -6,15 +6,15 @@ public class InputMain {
 	 public static void main(String[] argsArray) throws Exception {
 	        InputArgs args = new InputArgs( argsArray );
 	       
-	        
+	        try {
 	        int port = args.getNumber( "port", InputServer.DEFAULT_PORT ).intValue();
 	        String sdir = args.get("sdir", "/var/www");
 	        System.out.println("sdir" + sdir);
 	        final String baseUri = InputServer.getLocalhostBaseUri( port );
 	        System.out.println(String.format("Running server at [%s]", baseUri));
 	      //  ArchiveConfig.loadConfigFile();
-	         InputServer.INSTANCE.startServer( port,sdir );
-	        System.out.println("Press Ctrl-C to kill the server");
+	        // InputServer.INSTANCE.startServer( port,sdir );
+	        //System.out.println("Press Ctrl-C to kill the server");
 	        
 	       
 	       
@@ -35,11 +35,14 @@ public class InputMain {
 	            }
 	        } );
 	    
-	 try {
+	        InputServer.INSTANCE.startServer( port,sdir );
+            System.out.println("Press Ctrl-C to kill the server");
+
+            Thread.currentThread().join();
 		 System.out.println(String.format("Jersey app started with WADL available at "
                  + "%sapplication.wadl\nTry out %stest\nHit enter to stop it...",
                  baseUri, baseUri));
-	         System.in.read();
+	        // System.in.read();
 	 }
 	 finally {
          InputServer.INSTANCE.stopServer();
